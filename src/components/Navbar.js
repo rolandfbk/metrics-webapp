6f62a21a66603fdc4a104e6dd78b5aa78c26d2d1
rolandfbk <rolandfbk@gmail.com>
2022-06-22 11:36:54 +0200

    back button implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faMicrophone, faCog } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => (
-
-  <header>
-    <nav>
-      <div>
-        <span className="leftNav">
-          <FontAwesomeIcon icon={faChevronLeft} />
-          Africa
-        </span>
-      </div>
-      <div>
-        <span className="middleNav">Directory</span>
-      </div>
-      <div>
-        <span className="rightNav">
-          <FontAwesomeIcon icon={faMicrophone} />
-          <FontAwesomeIcon icon={faCog} />
-        </span>
-      </div>
-    </nav>
-  </header>
-);
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <header>
+      <nav>
+        <div aria-hidden onClick={() => navigate(-1)}>
+          <span className="leftNav">
+            <FontAwesomeIcon icon={faChevronLeft} />
+            Africa
+          </span>
+        </div>
+        <div>
+          <span className="middleNav">Directory</span>
+        </div>
+        <div>
+          <span className="rightNav">
+            <FontAwesomeIcon icon={faMicrophone} />
+            <FontAwesomeIcon icon={faCog} />
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
