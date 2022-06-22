3498a4898c191f0c18e036e08a8c7842c8e2e483
rolandfbk <rolandfbk@gmail.com>
2022-06-22 13:54:57 +0200

    testing done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from 'react-redux';
+import Metrics from './components/Metrics';
+import CountryDetails from './components/CountryDetails';
+import store from './redux/ConfigureStore';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('component test', () => {
+  const MetricsProvider = () => (
+    <Provider store={store}>
+      <Metrics />
+    </Provider>
+  );
+
+  const CountryDetailsProvider = () => (
+    <Provider store={store}>
+      <CountryDetails />
+    </Provider>
+  );
+
+  it('Metrics renders correctly', () => {
+    const tree = renderer.create(<MetricsProvider />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Simulation of user click on Morocco', () => {
+    const tree = renderer.create(<CountryDetailsProvider />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
