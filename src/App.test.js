ec714b2f8034d907bc8f7bf9b1edd839bf386d25
rolandfbk <rolandfbk@gmail.com>
2022-06-22 14:24:54 +0200

    Readmefile updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Metrics from './components/Metrics';
 import CountryDetails from './components/CountryDetails';
